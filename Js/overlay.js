a9134c65b23d3524ddc2e73d5795fc99c2669c83
Vi <kinjeks@gmail.com>
2021-03-20 20:08:44 +0300

    Add section products

--- Js/overlay.js
+++ Js/overlay.js
@@ -8,10 +8,12 @@ headerMenu.addEventListener('click', e =>{
   if(headerMenu.classList.contains('header-mobile-active')){
     overlay.style.opacity = '1'
     overlay.style.right = '0px'
+    overlay.style.display = 'flex'
     body.style.overflow = 'hidden';
   }
   else{
     overlay.style.opacity = '0'
+    overlay.style.display = 'none'
     overlay.style.right = '-9999px'
     body.style.overflow = 'visible';
   }
