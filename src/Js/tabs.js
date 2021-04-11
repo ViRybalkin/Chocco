0313287b91eaee5dec5041ba6d53e28a49669f24
Vi <kinjeks@gmail.com>
2021-04-11 14:07:54 +0300

    Add src

---
+++ src/Js/tabs.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll('.review__item');
+const buttons = document.querySelectorAll('.interactive__avatar');
+const link = document.querySelector('.interactive__avatar__link')
+
+buttons.forEach((element, index) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault()
+    const $this = element;
+    buttons.forEach((button, i) => {
+      if (button !== $this) {
+        button.classList.remove('interactive__avatar-active');
+        tabs[i].classList.remove('review__item-active');
+      }
+      
+    });
+    $this.classList.add('interactive__avatar-active');
+    tabs[index].classList.add('review__item-active');
+  })
+})
